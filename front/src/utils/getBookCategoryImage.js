e2840d036b2fa6805e196ed3186c510c26e8ab28
karim-emad90 <karim.sharkawy90@gmail.com>
2026-04-14 01:40:13 +0200

    fix cart image bug

--- front/src/utils/getBookCategoryImage.js
+++ front/src/utils/getBookCategoryImage.js
@@ -1,21 +1,18 @@
 import defaultBookImage from "../assets/category/default.jpg";
 
-export function getBookImage(book) {
+export const getBookImage = (book) => {
   const base = (import.meta.env.VITE_API_URL || "").replace(/\/$/, "");
 
-  const fileName = book?.coverImageUrl?.trim();
+  const fileName =
+    book?.coverImageUrl ||
+    book?.attributes?.coverImageUrl ||
+    book?.data?.coverImageUrl;
 
-  console.log("BOOK:", book);
-  console.log("BASE URL:", base);
-  console.log("FILE NAME:", fileName);
-
-  if (!fileName) {
+  if (!fileName || typeof fileName !== "string") {
     return defaultBookImage;
   }
 
-  if (fileName.startsWith("http")) {
-    return fileName;
-  }
+  if (fileName.startsWith("http")) return fileName;
 
   return `${base}/category-images/${fileName}`;
-}
+};
