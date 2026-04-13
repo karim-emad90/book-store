2532ed240c4fdea5868fc8efbc307d0ea1caaa7e
karim-emad90 <karim.sharkawy90@gmail.com>
2026-04-14 00:31:29 +0200

    fix missing coverImageUrl field

--- front/src/utils/getBookCategoryImage.js
+++ front/src/utils/getBookCategoryImage.js
@@ -1,20 +1,13 @@
-import defaultBookImage from "../assets/category/default.jpg";
-
-const API_URL = import.meta.env.VITE_API_URL?.replace(/\/$/, "");
-
 export function getBookImage(book) {
-  if (!book) return defaultBookImage;
+  const fileName =
+    book?.coverImageUrl ||
+    book?.attributes?.coverImageUrl;
 
-  const fileName = book?.coverImageUrl;
-
-  // 👇 مهم جدًا
   if (!fileName || typeof fileName !== "string") {
     return defaultBookImage;
   }
 
-  // 👇 لو URL كامل
   if (fileName.startsWith("http")) return fileName;
 
-  // 👇 ده المسار الصح
   return `${API_URL}/category-images/${fileName}`;
 }
